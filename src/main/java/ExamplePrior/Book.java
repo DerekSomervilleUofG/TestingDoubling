8cb41acbc0afb266fdad5f7112e5ff7f67a572d3
Derek Somerville <47667752+DerekSomerville@users.noreply.github.com>
2021-01-21 13:07:50 +0000

    Number of pages

--- src/main/java/ExamplePrior/Book.java
+++ src/main/java/ExamplePrior/Book.java
@@ -5,33 +5,25 @@ public class Book {
     private String author;
     private String title;
     private int releaseYear;
+    private int numberOfPages = 250;
     final String authorPrefix = "Author:";
     final String titlePrefix = "Title:";
     final String releaseYearPrefix = "Release Year:";
+    final String numberOfPagesPrefix = "Number of Pages:";
 
 
-    Book(String author, String title, int releaseYear){
+    Book(String author, String title, int releaseYear, int numberOfPages){
         this.author = author;
         this.title = title;
         this.releaseYear = releaseYear;
+        this.numberOfPages = numberOfPages;
     }
 
-    public String getAuthor(){
-        return author;
-    }
-
-    public String getTitle(){
-        return title;
-    }
-
-    public int getReleaseYear(){
-        return releaseYear;
-    }
-
-    private String bookInformation(){
-        String info = authorPrefix + " " + author;
-        info += " " + titlePrefix + " " + title;
-        info += " " + releaseYearPrefix + " " + releaseYear;
+    protected String bookInformation(){
+        String info = authorPrefix + " " + this.author;
+        info += " " + titlePrefix + " " + this.title;
+        info += " " + releaseYearPrefix + " " + this.releaseYear;
+        info += " " + numberOfPagesPrefix + " " + this.numberOfPages;
         return info;
     }
 
